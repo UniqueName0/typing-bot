78dfed64001d96a82315aea70390be8012d6fd41
name37566 <75515676+name37566@users.noreply.github.com>
2020-12-09 21:44:34 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,33 @@
+import os
+import discord
+from discord.ext import commands, tasks
+
+token = os.getenv('token')
+
+helptext = open('help.txt', 'r').read()
+
+
+
+bot = commands.Bot(command_prefix='TB')
+
+bot.remove_command('help')
+
+
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+
+@bot.command()
+async def help(ctx):
+    await ctx.send(helptext)
+
+@bot.command()
+async def start(ctx):
+    await ctx.channel.startTyping()
+
+@bot.command()
+async def stop(ctx):
+    await ctx.channel.stopTyping()
